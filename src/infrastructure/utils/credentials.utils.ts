ae6ac4de4127beaf2cd93166c9e28b2f26d2cadf
Francisco Solis <imfran@duck.com>
2023-07-07 23:31:26 -0400

    feat(puppeteer-upgrade): se han actualizado algunos archivos
    * Se han actualizado archivos que requerían de modelos antiguos de puppeteer

--- src/infrastructure/utils/credentials.utils.ts
+++ src/infrastructure/utils/credentials.utils.ts
@@ -1,125 +1,110 @@
 import jwt from "jsonwebtoken";
-import { Cookie, SetCookie } from "puppeteer";
 import Usuario from "../../core/models/usuario.model";
 
 export default class CredentialsUtils {
-  public static getMiUtemCookies(token: string): SetCookie[] {
-    const cookies: string = token.split("|")[0];
-    const sessionId: string = cookies.slice(0, 32);
-    const csrfToken: string = cookies.slice(32);
+    public static getMiUtemCookies(token: string) {
+        const cookies: string = token.split("|")[0];
+        const sessionId: string = cookies.slice(0, 32);
+        const csrfToken: string = cookies.slice(32);
 
-    let date = new Date();
-    date.setDate(date.getDate() + 364);
+        let date = new Date();
+        date.setDate(date.getDate() + 364);
 
-    return [
-      {
-        name: "sessionid",
-        value: sessionId,
-        domain: "mi.utem.cl",
-        path: "/",
-        expires: -1,
-        httpOnly: true,
-        secure: false,
-        session: true,
-        sameSite: "Lax",
-      },
-      {
-        name: "MIUTEM",
-        value: "miutem1",
-        domain: "mi.utem.cl",
-        path: "/",
-        expires: -1,
-        httpOnly: false,
-        secure: false,
-        session: true,
-      },
-      {
-        name: "csrftoken",
-        value: csrfToken,
-        domain: "mi.utem.cl",
-        path: "/",
-        expires: date.getTime() / 1000,
-        httpOnly: false,
-        secure: false,
-        session: false,
-        sameSite: "Lax",
-      },
-      {
-        name: "dialogShown",
-        value: "0",
-        domain: "mi.utem.cl",
-        path: "/",
-        expires: date.getTime() / 1000,
-        httpOnly: false,
-        secure: false,
-        session: false,
-      },
-    ];
-  }
-
-  public static getSigaUser(sigaBearerToken: string): Usuario | null {
-    if (sigaBearerToken != null) {
-      const sigaPayload: any = jwt.decode(sigaBearerToken);
-
-      return {
-        nombres: sigaPayload.given_name,
-        apellidos: sigaPayload.family_name,
-        nombreCompleto: sigaPayload.name,
-        correoUtem: sigaPayload.email,
-        username: sigaPayload.preferred_username,
-      };
-    }
-  }
-
-  public static getSigaToken(token: string): string | undefined {
-    return token.split("|")[1];
-  }
-
-  public static createToken(
-    sigaBearerToken: string,
-    miUtemCookies: Cookie[]
-  ): string {
-    let sessionId: string = "";
-    let csrfToken: string = "";
-
-    if (miUtemCookies) {
-      for (const cookie of miUtemCookies) {
-        if (cookie.name == "sessionid") {
-          sessionId = cookie.value;
-        }
-        if (cookie.name == "csrftoken") {
-          csrfToken = cookie.value;
-        }
-      }
+        return [
+            {
+                name: "sessionid",
+                value: sessionId,
+                domain: "mi.utem.cl",
+                path: "/",
+                expires: -1,
+                httpOnly: true,
+                secure: false,
+                session: true,
+                sameSite: "Lax",
+            },
+            {
+                name: "MIUTEM",
+                value: "miutem1",
+                domain: "mi.utem.cl",
+                path: "/",
+                expires: -1,
+                httpOnly: false,
+                secure: false,
+                session: true,
+            },
+            {
+                name: "csrftoken",
+                value: csrfToken,
+                domain: "mi.utem.cl",
+                path: "/",
+                expires: date.getTime() / 1000,
+                httpOnly: false,
+                secure: false,
+                session: false,
+                sameSite: "Lax",
+            },
+            {
+                name: "dialogShown",
+                value: "0",
+                domain: "mi.utem.cl",
+                path: "/",
+                expires: date.getTime() / 1000,
+                httpOnly: false,
+                secure: false,
+                session: false,
+            },
+        ];
     }
 
-    return sessionId + csrfToken + "|" + sigaBearerToken;
-  }
+    public static getSigaUser(sigaBearerToken: string): Usuario | null {
+        if (sigaBearerToken != null) {
+            const sigaPayload: any = jwt.decode(sigaBearerToken);
 
-  public static get emptyCookies(): SetCookie[] {
-    return [
-      {
-        name: "sessionid",
-        value: "",
-        domain: "mi.utem.cl",
-        path: "/",
-        expires: -1,
-        httpOnly: true,
-        secure: false,
-        session: true,
-        sameSite: "Lax",
-      },
-      {
-        name: "csrftoken",
-        value: "",
-        domain: "mi.utem.cl",
-        path: "/",
-        expires: -1,
-        httpOnly: false,
-        secure: false,
-        session: false,
-        sameSite: "Lax",
-      },
-    ];
-  }
+            return {
+                nombres: sigaPayload.given_name,
+                apellidos: sigaPayload.family_name,
+                nombreCompleto: sigaPayload.name,
+                correoUtem: sigaPayload.email,
+                username: sigaPayload.preferred_username,
+            };
+        }
+    }
+
+    public static getSigaToken(token: string): string | undefined {
+        return token.split("|")[1];
+    }
+
+    public static createToken(sigaBearerToken: string, miUtemCookies): string {
+        let sessionId: string = miUtemCookies.find((cookie: any) => cookie.name == "sessionid")?.value || "";
+        let csrfToken: string = miUtemCookies.find((cookie: any) => cookie.name == "csrftoken")?.value || "";
+
+        return sessionId + csrfToken + "|" + sigaBearerToken;
+    }
+
+    public static get emptyCookies(): object {
+        return [
+            {
+                name: "sessionid",
+                value: "",
+                domain: "mi.utem.cl",
+                path: "/",
+                expires: -1,
+                httpOnly: true,
+                secure: false,
+                session: true,
+                sameSite: "Lax",
+            },
+            {
+                name: "csrftoken",
+                value: "",
+                domain: "mi.utem.cl",
+                path: "/",
+                expires: -1,
+                httpOnly: false,
+                secure: false,
+                session: false,
+                sameSite: "Lax",
+            },
+        ];
+    }
 }
