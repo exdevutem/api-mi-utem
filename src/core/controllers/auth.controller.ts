a2712ee4c7cf283d904e4bf31caacf1eef961fb0
Francisco Solis <imfran@duck.com>
2023-08-26 12:47:32 -0400

    patch(errores-y-formato): arreglado algunos errores y formato.
    * Arreglado error que impedía iniciar el backend
    * Arreglado formato de nombres en auth.controller.ts
    * Mejorado creación de tokens.

--- src/core/controllers/auth.controller.ts
+++ src/core/controllers/auth.controller.ts
@@ -14,44 +14,53 @@ export class AuthController {
       const correo: string = req.body.correo;
       const contrasenia: string = req.body.contrasenia;
 
-      const academiaCookies = await AcademiaUserService.loginAndGetCookies(correo, contrasenia)
-
-      const usuarioSiga = await SigaApiAuthService.loginAndGetProfile(correo, contrasenia);
-      const sigaToken = usuarioSiga.token;
+      /* Inicia sesión en Siga */
+      const usuarioSigaByAuth = await SigaApiAuthService.loginAndGetProfile(correo, contrasenia); // Usuario generado por autenticación
+      const sigaToken = usuarioSigaByAuth.token;
 
       if (!sigaToken) {
         throw GenericError.SIGA_UTEM_ERROR
       }
 
-      let cookies: Cookie[];
+      /* Inicia sesión en Academia.UTEM */
+      let academiaCookies: Cookie[];
       try {
-        cookies = await MiUtemAuthService.loginAndGetCookies(correo, contrasenia);
+        academiaCookies = await AcademiaUserService.loginAndGetCookies(correo, contrasenia)
+      } catch (error) {
+        console.log("No se pudo loggear en Academia");
+      }
+
+      /* Inicia sesión en Mi.UTEM */
+      let miUtemCookies: Cookie[];
+      try {
+        miUtemCookies = await MiUtemAuthService.loginAndGetCookies(correo, contrasenia);
       } catch (error) {
         console.log("No se pudo loggear en Mi UTEM");
       }
 
-      const usuarioToken: Usuario = CredentialsUtils.getSigaUser(sigaToken);
+      /* Crea usuario y asigna un token para futuro uso de autorización */
+      const usuarioSigaByToken: Usuario = CredentialsUtils.getSigaUser(sigaToken); // Usuario generado por jwt (json web token)
+      const usuario: Usuario = {...usuarioSigaByAuth, ...usuarioSigaByToken};
+      usuario.token = CredentialsUtils.createToken(sigaToken, miUtemCookies, academiaCookies);
 
-      const usuario: Usuario = {...usuarioSiga, ...usuarioToken};
-      usuario.token = CredentialsUtils.createToken(sigaToken, cookies, academiaCookies);
-
-      if (usuarioSiga.perfiles?.length == 0) {
+      /* Valida que el usuario tenga un rol */
+      if (usuarioSigaByAuth.perfiles?.length == 0) {
         throw GenericError.SIN_ROL
       }
 
-      if (!usuarioSiga.perfiles.includes("Estudiante")) {
+      /* Valida que el usuario sea estudiante */
+      if (!usuarioSigaByAuth.perfiles.includes("Estudiante")) {
         throw GenericError.NO_ESTUDIANTE
       }
 
-      if (cookies) {
+      /* Asigna datos obtenidos utilizando el perfil Mi.UTEM */
+      if (miUtemCookies) {
         try {
-          let usuarioMiUtem: Usuario = await MiUtemUserService.getProfile(cookies);
-          usuario = {
-            ...usuario,
-            ...{
-              fotoUrl: usuarioMiUtem?.fotoUrl,
-            }
-          };
+          const usuarioMiUtem: Usuario = await MiUtemUserService.getProfile(miUtemCookies);
+          usuario.perfiles = usuarioMiUtem?.perfiles || usuario.perfiles;
+          usuario.nombreCompleto = usuarioMiUtem?.nombreCompleto || usuario.nombreCompleto;
+          usuario.rut = usuarioMiUtem?.rut || usuario.rut;
+          usuario.fotoUrl = usuarioMiUtem?.fotoUrl || usuario.fotoUrl;
         } catch (error) {
           console.log("No se pudo obtener el perfil de Mi UTEM");
         }
